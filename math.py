21a663f8cc6f0da78ed357e85fa95d040a397053
hansikajain <99399362+hansikajain@users.noreply.github.com>
2022-02-10 17:01:05 +0530

    Initial commit on math.py file

---
+++ math.py
@@ -0,0 +1,18 @@
+# Add
+def add(x,y):
+    pass
+# Subtract
+def subtract(x,y):
+    return x-y      #on master branch
+# Mutliply
+def mutiply(x,y):
+    return x*y      #On bug456 branch
+# Divide
+def divide(x,y):        #on bug 789
+    if(y==0):
+        return DIVIDE_BY_ZERO_ERROR
+
+    else:
+        return x/y 
+def square(x):
+    return x*x
